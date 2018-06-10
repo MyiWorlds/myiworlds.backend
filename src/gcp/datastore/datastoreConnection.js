f5899e2106014299257dfdadac7ca4587507cee1
Davey Edwards <davey_010@hotmail.com>
2018-06-09 17:03:16 -0700

    Get your comments out of here

--- src/gcp/datastore/datastoreConnection.js
+++ src/gcp/datastore/datastoreConnection.js
@@ -1,15 +1,9 @@
-// import GCPConfig from '../config/settings';
-
 const credentials = process.env.GCP_DATASTORE_OWNER
   ? JSON.parse(process.env.GCP_DATASTORE_OWNER)
   : {};
 
 const datastoreClient = require('@google-cloud/datastore')({
-  // keyFilename: GCPConfig.datastore.ownerServiceKeyPath,
   credentials,
-  // Only used for seeding the database to create the necessary entities
-  // Not sure if I will keep
-  // keyFilename: GCPConfig.datastore.ownerServiceKeySeedPath,
 });
 
 export default datastoreClient;
